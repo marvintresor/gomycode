8939661086e096ac3f275578b9fba1a6673af247
maiga tresor <maigaaligomy@gmail.com>
2023-10-31 12:24:29 +0100

    cheickpoint html

---
+++ Gomycode/res/js/js.js
@@ -0,0 +1,16 @@
+function connexion(nom,prenom){
+    var nombase="Koffi";
+    var prenombase="jean";
+    if(nombase !== nom && prenombase !==prenom){
+     console.log("Identifaicati ou pssd error");}
+
+    else{console.log("connexion réussie");
+        }
+    
+}
+connexion("koffi","jean");
+let list=[1,"jean","paul",20,true]
+for(let x=0;x< list.length; x ++){
+    console.log( list [x]  );
+
+}
